3a205656750e6b56e1752c4e945218f51a7d9290
tuncaybasak <tuncaybasak@gmail.com>
2020-10-20 17:43:06 +0300

    Update 012_functions.ts

--- 012_functions.ts
+++ 012_functions.ts
@@ -8,28 +8,28 @@ display(); //Output: Hello TypeScript
 
 // Functions can also include parameter types and return type.
 function Sum(x: number, y: number) : number {
-    return x + y;
+   return x + y;
 }
 Sum(2,3); // returns 5
 
 // == Anonymous Function ==
 // An anonymous function is one which is defined as an expression. This expression is stored in a variable. So, the function itself does not have a name.
 let greeting = function() {
-    console.log("Hello TypeScript!");
+  console.log("Hello TypeScript!");
 };
 greeting(); //Output: Hello TypeScript! 
 
 // An anonymous function can also include parameter types and return type.
 let Sum = function(x: number, y: number) : number
 {
-    return x + y;
+  return x + y;
 }
 Sum(2,3); // returns 5
 
 // == Optional Parameters ==
 // TypeScript has an optional parameter functionality. The parameters that may or may not receive a value can be appended with a '?' to mark them as optional.
 function Greet(greeting: string, name?: string ) : string {
-    return greeting + ' ' + name + '!';
+   return greeting + ' ' + name + '!';
 }
 Greet('Hello','Steve');//OK, returns "Hello Steve!"
 Greet('Hi'); // OK, returns "Hi undefined!".
@@ -40,7 +40,7 @@ Greet('Hi','Bill','Gates'); //Compiler Error: Expected 2 arguments, but got 3.
 // will initialize the parameter with the default value
 
 function Greet(name: string, greeting: string = "Hello") : string {
-    return greeting + ' ' + name + '!';
+  return greeting + ' ' + name + '!';
 }
 Greet('Steve');//OK, returns "Hello Steve!"
 Greet('Steve', 'Hi'); // OK, returns "Hi Steve!".
@@ -48,11 +48,8 @@ Greet('Bill'); //OK, returns "Hello Bill!"
 
 // When the default parameters precede required parameters in a function, they can be called by passing undefined.
 function Greet(greeting: string = "Hello", name: string) : string {
-    return greeting + ' ' + name + '!';
+  return greeting + ' ' + name + '!';
 }
 Greet(undefined, 'Steve');//returns "Hello Steve!"
 Greet("Hi", 'Steve'); //returns "Hi Steve!".
 Greet(undefined, 'Bill'); //returns "Hello Bill!"
-
-
-
