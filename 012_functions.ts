95b176879c0a0acc4ebb5956cd6c647674c8a9e5
tuncaybasak <tuncaybasak@gmail.com>
2020-10-20 17:49:56 +0300

    Update 012_functions.ts

--- 012_functions.ts
+++ 012_functions.ts
@@ -53,3 +53,30 @@ function Greet(greeting: string = "Hello", name: string) : string {
 Greet(undefined, 'Steve');//returns "Hello Steve!"
 Greet("Hi", 'Steve'); //returns "Hi Steve!".
 Greet(undefined, 'Bill'); //returns "Hello Bill!"
+
+
+// == Function Overloading ==
+// TypeScript provides the concept of function overloading. You can have multiple functions with the same name but different parameter types and return type. 
+// However, the number of parameters should be the same.
+
+function add(a:string, b:string):string;
+
+function add(a:number, b:number): number;
+
+function add(a: any, b:any): any {
+    return a + b;
+}
+
+add("Hello ", "Steve"); // returns "Hello Steve" 
+add(10, 20); // returns 30 
+
+// Function overloading with different number of parameters and types with same name is not supported.
+function display(a:string, b:string):void //Compiler Error: Duplicate function implementation
+{
+    console.log(a + b);
+}
+
+function display(a:number): void //Compiler Error: Duplicate function implementation
+{
+    console.log(a);
+}
