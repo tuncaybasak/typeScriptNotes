91cc24e229070be82fa2f8ef41600bb3b792aed6
tuncaybasak <tuncaybasak@gmail.com>
2020-10-20 17:32:23 +0300

    Update 011_conditonals.ts

--- 011_conditonals.ts
+++ 011_conditonals.ts
@@ -1,5 +1,4 @@
 // ## if else ##
-
 let x: number = 10, y = 20;
 if (x > y) 
 {
@@ -14,18 +13,14 @@ else if (x == y)
     console.log('x is equal to y');
 }
 
-
 // ## Ternary operator ##
 // It checks for a boolean condition and executes one of the two statements, depending on the result of the boolean condition.
-
 let x: number = 10, y = 20;
 x > y? console.log('x is greater than y.'): console.log('x is less than or equal to y.')
-
 
 // ## switch ##
 
 let day : number = 4;
-
 switch (day) {
     case 0:
         console.log("It is a Sunday.");
@@ -38,18 +33,14 @@ switch (day) {
         break;
 }
 
-
 // ## TypeScript - for Loops ##
-
 //for Loop
-
 for (let i = 0; i < 3; i++) {
   console.log ("Block statement execution no." + i);
 }
 
 // for...of Loop
 // TypeScript includes the for...of loop to iterate and access elements of an array, list, or tuple collection
-
 let arr = [10, 20, 30, 40];
 for (var val of arr) {
   console.log(val); // prints values: 10, 20, 30, 40
@@ -62,14 +53,12 @@ for (var char of str) {
 
 // for...in Loop
 // This can be used with an array, list, or tuple. The for...in loop iterates through a list or collection and returns an index on each iteration.
-
 let arr = [10, 20, 30, 40];
 for (var index in arr) {
   console.log(index); // prints indexes: 0, 1, 2, 3
 
   console.log(arr[index]); // prints elements: 10, 20, 30, 40
 }
-
 
 // ## while Loop ##
 let i: number = 2;
