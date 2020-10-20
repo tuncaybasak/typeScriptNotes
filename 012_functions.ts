dfeee5f93581d82c4eb53bc78818b4c4dcddf0e7
tuncaybasak <tuncaybasak@gmail.com>
2020-10-20 17:36:30 +0300

    Create 012_functions.ts

---
+++ 012_functions.ts
@@ -0,0 +1,16 @@
+// Named Functions
+// A named function is one where you declare and call a function by its given name.
+
+function display() {
+  console.log("Hello TypeScript!");
+}
+display(); //Output: Hello TypeScript 
+
+// Functions can also include parameter types and return type.
+unction Sum(x: number, y: number) : number {
+    return x + y;
+}
+Sum(2,3); // returns 5
+
+
+
