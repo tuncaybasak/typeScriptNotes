81e551529ebc9c3be0b9eaab680d29eec5838625
tuncaybasak <tuncaybasak@gmail.com>
2020-10-21 09:19:07 +0300

    Update 016_abstractClass.ts

--- 016_abstractClass.ts
+++ 016_abstractClass.ts
@@ -34,3 +34,27 @@ let emp: Person = new Employee("James", 100);
 emp.display(); //James
 
 let emp2: Person = emp.find('Steve');
+
+// The abstract class can also include an abstract property, as shown below.
+abstract class Person {
+    abstract name: string;
+
+    display(): void{
+        console.log(this.name);
+    }
+}
+
+class Employee extends Person { 
+    name: string;
+    empCode: number;
+    
+    constructor(name: string, code: number) { 
+        super(); // must call super()
+        
+        this.empCode = code;
+        this.name = name;
+    }
+}
+
+let emp: Person = new Employee("James", 100);
+emp.display(); //James
