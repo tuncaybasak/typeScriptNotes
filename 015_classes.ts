0b10d98dc20aba2f28cbbf4c1bd654448d79e8f9
tuncaybasak <tuncaybasak@gmail.com>
2020-10-21 09:12:23 +0300

    Update 015_classes.ts

--- 015_classes.ts
+++ 015_classes.ts
@@ -58,6 +58,95 @@ let emp = new Employee(100, "Bill");
 emp.displayName(); // Name = Bill, Employee Code = 100
 
 
+// Class Implements Interface
+// A class can implement single or multiple interfaces
+
+interface IPerson {
+    name: string;
+    display():void;
+}
+
+interface IEmployee {
+    empCode: number;
+}
+
+class Employee implements IPerson, IEmployee {
+    empCode: number;
+    name: string;
+    
+    constructor(empcode: number, name:string) {
+        this.empCode = empcode;
+        this.name = name;
+    }
+    
+    display(): void {
+        console.log("Name = " + this.name +  ", Employee Code = " + this.empCode);
+    }
+}
+
+let per:IPerson = new Employee(100, "Bill");
+per.display(); // Name = Bill, Employee Code = 100
+
+let emp:IEmployee = new Employee(100, "Bill");
+emp.display(); //Compiler Error: Property 'display' does not exist on type 'IEmployee'
+
+// Interface extends Class
+// An interface can also extend a class to represent a type.
+
+class Person {
+    name: string;
+}
+
+interface IEmployee extends Person { 
+    empCode: number;
+}
+
+let emp: IEmployee = { empCode  : 1, name:"James Bond" }
 
 
+// Method Overriding
+// When a child class defines its own implementation of a method from the parent class, it is called method overriding.
 
+class Car {
+    name: string;
+        
+    constructor(name: string) {
+        this.name = name;
+    }
+    
+    run(speed:number = 0) {
+        console.log("A " + this.name + " is moving at " + speed + " mph!");
+    }
+}
+
+class Mercedes extends Car {
+    
+    constructor(name: string) {
+        super(name);
+    }
+    
+    run(speed = 150) {
+        console.log('A Mercedes started')
+        super.run(speed);
+    }
+}
+
+class Honda extends Car {
+    
+    constructor(name: string) {
+        super(name);
+    }
+    
+    run(speed = 100) {
+        console.log('A Honda started')
+        super.run(speed);
+    }
+}
+
+let mercObj = new Mercedes("Mercedes-Benz GLA");
+let hondaObj = new Honda("Honda City")
+
+mercObj.run();  // A Mercedes started A Mercedes-Benz GLA is moving at 150 mph!
+hondaObj.run(); // A Honda started A Honda City is moving at 100 mph!
+
+// When we create objects of the child class and call the run() method on this object, it will call its own overridden method of run() and not that of the parent class.
