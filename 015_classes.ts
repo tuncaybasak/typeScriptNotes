59751eb583563bb9e7fdbb7cca8746e1c0608f12
tuncaybasak <tuncaybasak@gmail.com>
2020-10-21 09:06:48 +0300

    Update 015_classes.ts

--- 015_classes.ts
+++ 015_classes.ts
@@ -16,6 +16,9 @@ class Employee {
     }
 }
 
+// Creating an Object of Class
+let emp = new Employee(100,"Steve");
+
 // The TypeScript compiler will convert the above class to the following JavaScript code using closure
 var Employee = /** @class */ (function () {
     function Employee(name, code) {
@@ -27,3 +30,34 @@ var Employee = /** @class */ (function () {
     };
         return Employee;
 }());
+
+// Inheritance
+// Just like object-oriented languages such as Java and C#, TypeScript classes can be extended to create new classes with inheritance, using the keyword extends.
+class Person {
+    name: string;
+    
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+class Employee extends Person {
+    empCode: number;
+    
+    constructor(empcode: number, name:string) {
+        super(name);
+        this.empCode = empcode;
+    }
+    
+    displayName():void {
+        console.log("Name = " + this.name +  ", Employee Code = " + this.empCode);
+    }
+}
+
+let emp = new Employee(100, "Bill");
+emp.displayName(); // Name = Bill, Employee Code = 100
+
+
+
+
+
