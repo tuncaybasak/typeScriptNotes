55841c5ff90b527241ae0c797f7d164a109649af
tuncaybasak <tuncaybasak@gmail.com>
2020-10-21 15:12:54 +0300

    Update 010_typeAssertion.ts

--- 010_typeAssertion.ts
+++ 010_typeAssertion.ts
@@ -1,6 +1,5 @@
 // Type Assertion
 // Type assertion allows you to set the type of a value and tell the compiler not to infer it
-
 // There are two ways to do type assertion in TypeScript
 
 // 1. Using the angular bracket <> syntax. So far in this section, we have used angular brackets to show type assertion.
@@ -11,3 +10,19 @@ let employeeCode = <number> code;
 //Example: as syntax Copy
 let code: any = 123; 
 let employeeCode = code as number;
+
+// while dealing with JSX in TypeScript, only the as syntax is allowed
+
+// Type Assertion with Object
+interface Employee { 
+    name: string; 
+    code: number; 
+} 
+
+let employee = <Employee> { }; 
+employee.name = "John"; // OK
+employee.code = 123; // OK
+
+// exmp
+let employeeCode = <number> myJSLib.GetEmployeeCode('Steve');
+console.log(typeof(employeeCode)); // number
